8025f5ded4c3fee89d88da85452ca375b76747a3
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-04-30 11:35:53 +0200

    defined DataModels
    (LiveShare session)
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    Co-Authored-By: Gabriel Wedin <142593473+gabrielwedin@users.noreply.github.com>

---
+++ games-r-us-source/Models/Bid.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace games_r_us_source.Models
+{
+    public class Bid
+    {
+        public int ID {  get; set; }
+
+        [ForeignKey("ListingID")]
+        public int ListingID { get; set; }
+
+        public Listing Listing { get; set; }
+
+        // account that placed the bid 
+        [ForeignKey("AccountID")]
+        public int AccountID {  get; set; }
+
+        public Account Account { get; set; }
+
+        public decimal Amount { get; set; }
+
+        // time bid was placed
+        public DateTime Time { get; set; }
+    }
+}
---
+++ games-r-us-source/Models/Listing.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace games_r_us_source.Models
+{
+    public enum Category
+    {
+        Console,
+    }
+
+    public class Listing
+    {
+        public int ID { get; set; }
+
+        [ForeignKey("AccountID")]
+        public int AccountID { get; set; }
+
+        public Account Account { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal StartingPrice { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime AuctionEnd { get; set; }
+
+        public Category Category { get; set; }
+    }
+}

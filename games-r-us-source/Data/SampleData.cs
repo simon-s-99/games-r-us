030ef881ce1edf54be0666cca059bb50d886b494
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-04-30 14:13:15 +0200

    Added template for getting sampledata from JSON
    Template code from an old project that retrieves sample/dev-data from a .JSON file and saves to database.
    
    #5
    
    (LiveShare session)
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    Co-Authored-By: Gabriel Wedin <142593473+gabrielwedin@users.noreply.github.com>
    Co-Authored-By: Sam <142818774+Samuel-Loof@users.noreply.github.com>

---
+++ games-r-us-source/Data/SampleData.cs
@@ -0,0 +1,28 @@
+﻿using games_r_us_source.Data;
+using games_r_us_source.Models;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace groceries_webshop.Data
+{
+    public class SampleData
+    {
+        public static void Create(AppDbContext database)
+        {
+            // If no products are found, add sample products.
+            //if (!database.Products.Any())
+            //{
+            //    string filePath = "./Data/product-data.json";
+            //    string productData = File.ReadAllText(filePath);
+            //    Product[] products = JsonSerializer.Deserialize<Product[]>(productData);
+
+            //    for (int i = 0; i < products.Length; i++)
+            //    {
+            //        database.Products.Add(products[i]);
+            //    }
+
+            //    database.SaveChanges();
+            //}
+        }
+    }
+}

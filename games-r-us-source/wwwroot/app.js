4eeb936af0bb10129fc3b61cf106614c5bb12140
Sam <zaaamuel_96@hotmail.com>
2024-05-01 17:09:48 +0200

    CreateListing.cs initial commit
    Majority of it's functionality has been added, but still contains a lot of bugs, such as open/close button for create listing modal not working, and minimal error handling. Just very basic css added so far

---
+++ games-r-us-source/wwwroot/app.js
@@ -0,0 +1,9 @@
+function CloseModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'none';
+}
+
+//function showModal(modalId) {
+//    const modal = document.getElementById(modalId);
+//    modal.style.display = 'block'; 
+//}

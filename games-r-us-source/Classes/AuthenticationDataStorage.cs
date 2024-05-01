fdc1b711d1a0cdab67ccc434736cdc95691121a3
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-05-01 19:40:01 +0200

    Added nuget packages + tokenclass
    Added nuget packages necessary to authorize and receive account information. Added Class to hold token (for later log-in permanence).

---
+++ games-r-us-source/Classes/AuthenticationDataStorage.cs
@@ -0,0 +1,10 @@
+﻿namespace games_r_us_source.Classes
+{
+    // Class holds our users logged in token, for dev-purposes this
+    // is stored in volatile memory but a long term solution would be 
+    // localStorage or something similar. 
+    public class AuthenticationDataStorage
+    {
+        public string Token { get; set; } = "";
+    }
+}

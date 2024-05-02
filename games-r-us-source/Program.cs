b3d633d7faf9e915b23c803f48b770a0cae4df87
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-05-02 18:07:59 +0200

    Google OAuth login added to builder
    also saves user to our account table in db if they have not logged in before

--- games-r-us-source/Program.cs
+++ games-r-us-source/Program.cs
@@ -1,6 +1,10 @@
 using games_r_us_source.Components;
 using games_r_us_source.Data;
+using games_r_us_source.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace games_r_us_source
 {
@@ -17,6 +21,51 @@ namespace games_r_us_source
             // add db
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // below services (& chained calls) sets up our user authentication
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = "Google";
+            }).AddCookie(options =>
+            {
+                // When a user logs in to Google for the first time,
+                // create a local account for that user in our database.
+                options.Events.OnValidatePrincipal += async context =>
+                {
+                    var serviceProvider = context.HttpContext.RequestServices;
+                    using var db = new AppDbContext(serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>());
+
+                    string subject = context.Principal.FindFirstValue(ClaimTypes.NameIdentifier);
+                    string issuer = context.Principal.FindFirst(ClaimTypes.NameIdentifier).Issuer;
+                    string name = context.Principal.FindFirst(ClaimTypes.Name).Value;
+
+                    var account = db.Accounts
+                        .FirstOrDefault(p => p.OpenIDIssuer == issuer && p.OpenIDSubject == subject);
+
+                    if (account == null)
+                    {
+                        account = new Account
+                        {
+                            OpenIDIssuer = issuer,
+                            OpenIDSubject = subject,
+                            Name = name
+                        };
+                        db.Accounts.Add(account);
+                    }
+                    else
+                    {
+                        // If the account already exists, just update the name in case it has changed.
+                        account.Name = name;
+                    }
+
+                    await db.SaveChangesAsync();
+                };
+            }).AddGoogle(googleOptions =>
+            {
+                googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+                googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+            });
 
             var app = builder.Build();
 

00773f14f8ee4a694467ccf4e6bdd1a063e94ae1
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-05-02 22:19:52 +0200

    Last push to branch | DEAD BRANCH (see comment)
    This was a failed/convoluted way of implementing user authentication, this is being implemented on another branch instead (alternate-login).

--- games-r-us-source/Classes/GamesRUsAccountService.cs
+++ games-r-us-source/Classes/GamesRUsAccountService.cs
@@ -1,4 +1,6 @@
-﻿namespace games_r_us_source.Classes
+﻿using games_r_us_source.Models;
+
+namespace games_r_us_source.Classes
 {
     public class GamesRUsAccountService
     {
@@ -9,6 +11,21 @@
         {
             _httpClient = httpClient;
             _authenticationDataStorage = authenticationDataStorage;
+        }
+
+        public async Task<Account?> SendAuthenticateRequestAsync(string accountName)
+        {
+            var response = await _httpClient.GetAsync();
+
+            // true if we get a response in 200-299 range 
+            if (response.IsSuccessStatusCode)
+            {
+                string token = await response.Content.ReadAsStringAsync();
+                var claimPrincipal = CreateClaimsPrincipalFromToken(token);
+                var account = Account.FromClaimsPrincipal(claimPrincipal);
+            }
+
+            return null;
         }
     }
 }

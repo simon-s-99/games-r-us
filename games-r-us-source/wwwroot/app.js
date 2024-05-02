07ef6c645938482f8e1f7d6732e0a977c60b00aa
Sam <zaaamuel_96@hotmail.com>
2024-05-02 18:25:24 +0200

    Users can now upload images to their listings
    Images are currently saved to "D:\\Temp\\Storage"

--- games-r-us-source/wwwroot/app.js
+++
@@ -1,9 +0,0 @@
-function CloseModal(modalId) {
-    const modal = document.getElementById(modalId);
-    modal.style.display = 'none';
-}
-
-//function showModal(modalId) {
-//    const modal = document.getElementById(modalId);
-//    modal.style.display = 'block'; 
-//}

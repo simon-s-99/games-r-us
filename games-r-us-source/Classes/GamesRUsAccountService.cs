f286155ac509e8457212c53dfd6dabde057ce16f
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-05-02 13:52:13 +0200

    Create GamesRUsAccountService class + comment in App

---
+++ games-r-us-source/Classes/GamesRUsAccountService.cs
@@ -0,0 +1,14 @@
+﻿namespace games_r_us_source.Classes
+{
+    public class GamesRUsAccountService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AuthenticationDataStorage _authenticationDataStorage;
+
+        public GamesRUsAccountService(HttpClient httpClient, AuthenticationDataStorage authenticationDataStorage)
+        {
+            _httpClient = httpClient;
+            _authenticationDataStorage = authenticationDataStorage;
+        }
+    }
+}

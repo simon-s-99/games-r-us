8f4f26aeca6f6c0504e73ead227412e1fe70bf94
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-05-02 14:48:42 +0200

    Create GamesRUsAuthenticationStateProvider.cs

---
+++ games-r-us-source/Classes/GamesRUsAuthenticationStateProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace games_r_us_source.Classes
+{
+    public class GamesRUsAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly GamesRUsAccountService _gamesRUsAccountService;
+
+        public GamesRUsAuthenticationStateProvider(GamesRUsAccountService gamesRUsAccountService)
+        {
+            _gamesRUsAccountService = gamesRUsAccountService;
+        }
+
+        // temp code, to be implemented 
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

6681578c64ff38a698aae5c6e484705244078df4
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-05-02 14:25:25 +0200

    Account.cs | added Claims for user auth
    Added ClaimsPrincipal/ClaimsIdentity to Account class, this will be used for Authentication + Authorization.

--- games-r-us-source/Models/Account.cs
+++ games-r-us-source/Models/Account.cs
@@ -1,4 +1,6 @@
-﻿namespace games_r_us_source.Models
+﻿using System.Security.Claims;
+
+namespace games_r_us_source.Models
 {
     public class Account
     {
@@ -13,5 +15,18 @@
         public string? PhoneNumber { get; set; }
 
         public string? Address { get; set; }
+
+        // The following claimsprincipal is needed for user authentication when loggin in. 
+        public ClaimsPrincipal ToClaimsPrincipal() => new(
+            new ClaimsIdentity(
+                new Claim[]
+                {
+                    new(ClaimTypes.Name, this.Name),
+                }, "Games R Us"));
+
+        public static Account FromClaimsPrincipal(ClaimsPrincipal principal) => new()
+        {
+            Name = principal.FindFirst(ClaimTypes.Name)?.Value ?? "",
+        };
     }
 }

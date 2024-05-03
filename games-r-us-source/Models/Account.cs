41ad1a307af07492abf30edf3d8ce8dcf1e76886
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-05-03 23:07:10 +0200

    Last push to branch | DEAD BRANCH (see comment)
    We have to re-scaffold the project in main branch with the identity option selected to get a working login as no documentation mentions how to do this without it for .NET 8.
    
    The scaffold can not be added to this version of the program due to an unfixed Visual Studio error that's been around since .NET 6.

--- games-r-us-source/Models/Account.cs
+++ games-r-us-source/Models/Account.cs
@@ -1,4 +1,8 @@
-﻿namespace games_r_us_source.Models
+﻿using games_r_us_source.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace games_r_us_source.Models
 {
     public class Account
     {
@@ -13,5 +17,30 @@
         public string? PhoneNumber { get; set; }
 
         public string? Address { get; set; }
+
+        // v move this 
+
+        /// <summary>
+        /// </summary>
+        /// <param name="authState"></param>
+        /// <returns>The account associated with the AuthenticationState</returns>
+        //public Account? GetAccountFromAuthState(AuthenticationState authState)
+        //{
+        //    // exit method if user is not logged in 
+        //    if (authState == null) { return null; }
+
+        //    string username = authState.User.Identity.Name;
+
+        //    Account loggedInAccount;
+
+        //    using (var dbContext = new AppDbContext())
+        //    {
+
+        //    }
+
+        //    return loggedInAccount;
+        //    //IEnumerable<ClaimsIdentity> userIdentities = authState.User.Identities;
+        //    //IEnumerable<Claim> claims = authState.User.Claims;
+        //}
     }
 }

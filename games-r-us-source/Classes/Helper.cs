c52025b961bbf76bd874fbff6d4b6b9c863ffabf
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-05-04 23:32:22 +0200

    Fix bugs, style component

---
+++ games-r-us-source/Classes/Helper.cs
@@ -0,0 +1,21 @@
+﻿using games_r_us_source.Models;
+using games_r_us_source.Data;
+
+namespace games_r_us_source.Classes
+{
+    public class Helper
+    {
+
+        public static Account GetAccountFromAccountID(int accountID, AppDbContext context)
+        {
+            Account account = context.Accounts.Where(a => a.ID == accountID).First();
+            return account;
+        }
+
+        public static Listing GetListingFromListingID(int listingID, AppDbContext context)
+        {
+            Listing listing = context.Listings.Where(a => a.ID == listingID).First();
+            return listing;
+        }
+    }
+}

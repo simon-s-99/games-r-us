bf859009fcc544cb4c6561be0245f7ade969e5a9
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-05-04 12:46:19 +0200

    Changes from pr #19 added + RootNameSpace changed
    Main is now up to date with pr #19
    (commit: 0907c9084b63fd477592d3f6d876e18dbaa3ef26 )

--- games-r-us-source/Data/ApplicationDbContext.cs
+++ games-r-us-source/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using games_r_us_source.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,5 +6,10 @@ namespace games_r_us_source.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Account> Accounts { get; set; }
+
+        public DbSet<Listing> Listings { get; set; }
+
+        public DbSet<Bid> Bids { get; set; }
     }
 }
---
+++ games-r-us-source/Models/Bid.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace games_r_us_source.Models
+{
+    public class Bid
+    {
+        public int ID {  get; set; }
+
+        [ForeignKey("ListingID")]
+        public int ListingID { get; set; }
+
+        // sets ListingID to null if the related listing is deleted
+        [DeleteBehavior(DeleteBehavior.NoAction)]
+        public Listing Listing { get; set; }
+
+        // account that placed the bid 
+        [ForeignKey("AccountID")]
+        public int AccountID {  get; set; }
+
+        // sets AccountID to null if the related account is deleted
+        [DeleteBehavior(DeleteBehavior.NoAction)]
+        public Account Account { get; set; }
+
+        public decimal Amount { get; set; }
+
+        // time bid was placed
+        public DateTime Time { get; set; }
+    }
+}

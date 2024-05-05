2d4998cb2d78c795912b3cb1253fb9eb9b35e54b
Simon Sörqvist <simon.t.sorqvist@gmail.com>
2024-05-05 12:22:04 +0200

    Login for new project scaffold (#23)
    * Moved Models to Data-folder | ApplicationUser replaces Account
    
    Moved the contents of the Models folder to Data (this follows microsofts "best-practice" apparently).
    
    Removed Account class and added it's properties to ApplicationUser which already contained a lot of similar functionality. This also makes it easier for us when we want to Authorize a user to perform CRUD operations on their own listings.
    
    * Changed Bid+Listing to accomodate ApplicationUser
    
    Bid and Listing datamodels are now updated to work with ApplicationUser.
    
    ApplicationUserID (formerly AccountID) was changed from int to string.
    
    Navigation property in bid & listing had to be marked [NotMapped] to get a proper migration, this might cause issues later.
    
    * Updated sampledata to work with new DataModels
    
    Updated sampledata to work with the updated versions of Bid, Listing & ApplicationUser (formerly Account).
    
    * Updated ListingDetails to reference ApplicationUser
    
    now references ApplicationUser instead of Account
    
    AppDbContext was also updated to acommodate this
    
    * Google sign-in (& -out) now works
    
    There is a lot of other functionality that we can utilise aswell, such as facebook login, twitter login or making an account just for this site.
    
    * ApplicationUser -> ApplicationUsers
    
    Renamed ApplicationUser to ApplicationUsers to rename consistent with our naming of other tables in the database.
    
    * Fix error: Forgot to rename one variable

--- games-r-us-source/Data/ApplicationUser.cs
+++ games-r-us-source/Data/ApplicationUser.cs
@@ -2,9 +2,15 @@ using Microsoft.AspNetCore.Identity;
 
 namespace games_r_us_source.Data
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
+	// Add profile data for application users by adding properties to the ApplicationUser class
+	public class ApplicationUser : IdentityUser
     {
+        // users first + last name
+        // FullName is nullable in case we can't set it with Cookie middleware 
+        public string? FullName { get; set; }
+        
+        // users address 
+        public string? Address { get; set; }
     }
 
 }
--- games-r-us-source/Program.cs
+++ games-r-us-source/Program.cs
@@ -5,78 +5,84 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Security.Claims;
 
 namespace games_r_us_source
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
+			// Add services to the container.
+			builder.Services.AddRazorComponents()
+				.AddInteractiveServerComponents();
 
-            builder.Services.AddCascadingAuthenticationState();
-            builder.Services.AddScoped<IdentityUserAccessor>();
-            builder.Services.AddScoped<IdentityRedirectManager>();
-            builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+			builder.Services.AddCascadingAuthenticationState();
+			builder.Services.AddScoped<IdentityUserAccessor>();
+			builder.Services.AddScoped<IdentityRedirectManager>();
+			builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
-            builder.Services.AddAuthentication(options =>
-                {
-                    options.DefaultScheme = IdentityConstants.ApplicationScheme;
-                    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-                })
-                .AddIdentityCookies();
+			builder.Services.AddAuthentication(options =>
+				{
+					options.DefaultScheme = IdentityConstants.ApplicationScheme;
+					options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+				})
+				.AddGoogle(googleOptions =>
+				{
+					googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+					googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+				})
+				.AddIdentityCookies();
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+			builder.Services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(connectionString));
+			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddSignInManager()
-                .AddDefaultTokenProviders();
+			builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+				.AddEntityFrameworkStores<ApplicationDbContext>()
+				.AddSignInManager()
+				.AddDefaultTokenProviders();
 
-            builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+			builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
-            var app = builder.Build();
+			var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseMigrationsEndPoint();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 
-            app.UseStaticFiles();
-            app.UseAntiforgery();
+			app.UseStaticFiles();
+			app.UseAntiforgery();
 
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
+			app.MapRazorComponents<App>()
+				.AddInteractiveServerRenderMode();
 
-            // Add additional endpoints required by the Identity /Account Razor components.
-            app.MapAdditionalIdentityEndpoints();
+			// Add additional endpoints required by the Identity /Account Razor components.
+			app.MapAdditionalIdentityEndpoints();
 
-            // adds sampledata for a table if the table is empty 
-            // DOES NOT add data for AspNet-tables
-            using (var scope = app.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var database = services.GetRequiredService<ApplicationDbContext>();
-                SampleData.Create(database);
-            }
+			// adds sampledata for a table if the table is empty 
+			// DOES NOT add data for AspNet-tables
+			using (var scope = app.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
+				var database = services.GetRequiredService<ApplicationDbContext>();
+				SampleData.Create(database);
+			}
 
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }

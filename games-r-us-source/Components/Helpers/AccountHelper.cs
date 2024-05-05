ac8db5985461abc8493f6bc54d3c580f47da4765
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-05-05 13:09:21 +0200

    PlaceBid branch with new scaffold, commits summarized
    This commit is a summary of the first 3 commits in "12-place-bid-on-listing"
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>

---
+++ games-r-us-source/Components/Helpers/AccountHelper.cs
@@ -0,0 +1,13 @@
+﻿using games_r_us_source.Data;
+
+namespace games_r_us_source.Components.Helpers
+{
+    public class AccountHelper
+    {
+        public static ApplicationUser GetAccountFromAccountID(string accountID, ApplicationDbContext context)
+        {
+            ApplicationUser account = context.ApplicationUsers.Where(a => a.Id == accountID).FirstOrDefault();
+            return account;
+        }
+    }
+}

13886e1f4247215798105c108090b9f05e599fce
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-05-06 10:50:16 +0200

    Nest component in ListingDetails

---
+++ games-r-us-source/Components/Helpers/BidHelper.cs
@@ -0,0 +1,13 @@
+﻿using games_r_us_source.Data;
+
+namespace games_r_us_source.Components.Helpers
+{
+    public class BidHelper
+    {
+        public static decimal GetHighestBidAmountFromListingID(int listingID, ApplicationDbContext context)
+        {
+            decimal bid = context.Bids.Where(b => b.ID == listingID).Max(b => b.Amount);
+            return bid;
+        }
+    }
+}

d2e37e71ee14e33a769d726aad1fc1882aca24e0
Sam <zaaamuel_96@hotmail.com>
2024-05-06 20:34:39 +0200

    Translated Css into Tailwind
    Changed back from
    
    [StringLength(maximumLength:50, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 50 characters")]
    
    to
    
    StringLength(50, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 50 characters")]
    
    Because i got errors with the former.

--- games-r-us-source/Data/Listing.cs
+++ games-r-us-source/Data/Listing.cs
@@ -41,13 +41,14 @@ namespace games_r_us_source.Data
         public int ID { get; set; }
 
         [ForeignKey("ApplicationUserID")]
-		public string ApplicationUserID { get; set; }
+        public string ApplicationUserID { get; set; }
 
-		[NotMapped]
+        [NotMapped]
         [DeleteBehavior(DeleteBehavior.NoAction)] // sets AccountID to null if the related account is deleted 
-		public ApplicationUser ApplicationUser { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
 
-        [StringLength(maximumLength:50, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 50 characters")]
+        [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 50 characters")]
         public string Name { get; set; }
 
         [Range(1, double.MaxValue, ErrorMessage = "Starting price must be at least 1")]
@@ -55,8 +56,8 @@ namespace games_r_us_source.Data
 
         public string? ImagePath { get; set; }
 
-		[MaxLength(1024, ErrorMessage = "Description cannot exceed 1024 characters")]
-		public string? Description { get; set; }
+        [MaxLength(1000, ErrorMessage = "Description cannot exceed 1000 characters")]
+        public string? Description { get; set; }
 
         public DateTime AuctionEnd { get; set; }
 

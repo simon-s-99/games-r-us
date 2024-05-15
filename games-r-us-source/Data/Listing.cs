8e0e29b65925e8474c68d2ab4ac9c39fed2b0f83
Sam <142818774+Samuel-Loof@users.noreply.github.com>
2024-05-15 15:01:34 +0200

    4 edit listing (#35)
    * Added Pseudo code and @usings/injects
    
    * Added html and css for editforms
    
    Also added method for handling images
    
    * StartingPrice and AuctionEnd shows as disabled
    
    Added usings and injects
    Added dbContextFactory to program.cs since it didn't exist in this branch
    
    * Updated usings
    
    * Added DisablePrice boolean, and successMessage
    
    * Added validationMessage to edit listing
    
    * Added error handling for name input
    
    * Edit button is now added to logged in users listings
    
    Added temporary testing methods since CreateListing hasn't been merged into main yet
    
    * Edits now gets saved to the database
    
    * Added Code from create listing for testing purposes
    
    * Working on connection editbutton to listingDetail
    
    * ListingEdit | Now edits it's associated listing in listingdetails
    
    ListingDetails now display an "edit listing" button that allows the user to edit that listing.
    
    * Added IsUserListingOwner helper from "delete-listing" branch
    
    * bugfix, called wrong method in helper IsUserListingOwner
    
    * ImagePath is now saved and displayed correctly
    
    * Added: Remove image button
    
    * Changed size on "edit" button
    
    * Added for attributes to labels and fixed typo
    
    * Price can no longer be changed once a bid has been made
    
    Also added "ShowconfirmCancel" call if the user presses outside of the ShowModal, and fixed typos for labels
    
    * Removed existingListing and changed successmessage from string to bool
    
    * Working on removing "extra" usings
    
    * Reformated code
    
    * Added dbContext.Update, commented out some code
    
    the commented out code is possibly redundant, evaluate
    
    Co-Authored-By: Sam <142818774+Samuel-Loof@users.noreply.github.com>
    
    * Removed redundant code. Added removeImages to CreateListing
    
    ---------
    
    Co-authored-by: simon-s-99 <142667693+simon-s-99@users.noreply.github.com>

--- games-r-us-source/Data/Listing.cs
+++ games-r-us-source/Data/Listing.cs
@@ -4,65 +4,67 @@ using Microsoft.EntityFrameworkCore;
 
 namespace games_r_us_source.Data
 {
-    public enum Platform
-    {
-        XBOX,
-        PlayStation,
-        GameCube,
-        NintendoSwitch,
-        NintendoWii,
-        NintendoDS,
-        Nintendo64,
-        GameBoy
-    }
+        public enum Platform
+        {
+                XBOX,
+                PlayStation,
+                GameCube,
+                NintendoSwitch,
+                NintendoWii,
+                NintendoDS,
+                Nintendo64,
+                GameBoy
+        }
 
-    public enum GameCategory
-    {
-        FPS,
-        Sports,
-        Fantasy,
-        RPG,
-        Sandbox,
-        Platformer,
-        MMO,
-        TurnBased,
-        FourX,
-        Strategy,
-        Simulator,
-        Action,
-        Horror,
-        Fighting,
-        Indie,
-        Survival
-    }
+        public enum GameCategory
+        {
+                FPS,
+                Sports,
+                Fantasy,
+                RPG,
+                Sandbox,
+                Platformer,
+                MMO,
+                TurnBased,
+                FourX,
+                Strategy,
+                Simulator,
+                Action,
+                Horror,
+                Fighting,
+                Indie,
+                Survival
+        }
 
-    public class Listing
-    {
-        public int ID { get; set; }
+        public class Listing
+        {
+                public int ID { get; set; }
 
-        [ForeignKey("ApplicationUserID")]
-        public string ApplicationUserID { get; set; }
+                [ForeignKey("ApplicationUserID")]
+                public string ApplicationUserID { get; set; }
 
-        [NotMapped]
-        [DeleteBehavior(DeleteBehavior.NoAction)] // sets AccountID to null if the related account is deleted 
-        public ApplicationUser ApplicationUser { get; set; }
+                [NotMapped]
+                [DeleteBehavior(DeleteBehavior.NoAction)] // sets AccountID to null if the related account is deleted 
+                public ApplicationUser ApplicationUser { get; set; }
 
-        [Required]
-        [StringLength(50, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 50 characters")]
-        public string Name { get; set; }
+                [Required]
+                [StringLength(50, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 50 characters")]
+                public string Name { get; set; }
 
-        [Range(1, double.MaxValue, ErrorMessage = "Starting price must be at least 1")]
-        public decimal StartingPrice { get; set; }
 
-        public string? ImagePath { get; set; }
+                [Range(1, double.MaxValue, ErrorMessage = "Starting price must be at least 1")] // Add max price?
+                public decimal StartingPrice { get; set; }
 
-        [MaxLength(1024, ErrorMessage = "Description cannot exceed 1024 characters")]
-        public string? Description { get; set; }
+                public string? ImagePath { get; set; }
 
-        public DateTime AuctionEnd { get; set; }
 
-        public Platform Platform { get; set; }
+                [MaxLength(1000, ErrorMessage = "Description cannot exceed 1000 characters")]
+                public string? Description { get; set; }
 
-        public GameCategory? GameCategory { get; set; }
-    }
+                public DateTime AuctionEnd { get; set; }
+
+                public Platform Platform { get; set; }
+
+                public GameCategory? GameCategory { get; set; }
+        }
 }

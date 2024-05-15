4554e636dc2685207f36f9597efc4609c75203cd
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-05-15 18:06:21 +0200

    Add methods to handle notifications
    Co-Authored-By: Simon Sörqvist <simon.t.sorqvist@gmail.com>

--- games-r-us-source/Hubs/NotificationsHub.cs
+++ games-r-us-source/Hubs/NotificationsHub.cs
@@ -1,28 +1,41 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
 
 namespace games_r_us_source.Hubs
 {
     public class NotificationsHub : Hub<INotificationClient>
     {
-		//public async Task TestMessage(string message)
-		//{
-		//	await Clients.All.RecieveNotification(message);
-		//}
-
         // Send a test notification when user connect to hub
         public override async Task OnConnectedAsync()
         {
             await Clients.Client(Context.ConnectionId).RecieveNotification(
-                "User-message = " + Context.ConnectionId);
+                "User-message = " + Context.ConnectionId +
+                " nameid= " /* + Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value*/);
+
+            var omegaTest = Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var monkaS = Context.User?.FindFirst(ClaimTypes.Name)?.Value;
+            string? user = Context.UserIdentifier;
 
             await base.OnConnectedAsync();
         }
 
-        //public async Task SendNotification(string message)
-        //{
-        //    await Clients.All.RecieveNotification(message);
-        //}
+        public async Task SendNotificationToAll(string message)
+        {
+            await Clients.All.RecieveNotification(message);
+        }
+
+        public async Task SendNotificationTo(string appUserID, string message)
+        {
+            //HubConnectionContext test = new();
+            //var appuserid = Context.User?.Identity.Name;
+            var omegaTest = Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            string? user = Context.UserIdentifier;
+            if (user is not null)
+            {
+                await Clients.User(user).RecieveNotification(message);
+            }
+        }
     }
 
     public interface INotificationClient

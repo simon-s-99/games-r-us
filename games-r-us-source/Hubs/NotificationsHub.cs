22f20513f7ef1de783925837ee3475a73b3074e0
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-05-17 16:32:55 +0200

    RecieveNotification -> RecieveNotificationAsync
    changed to async naming (it was already a task capable of being run asynchronously)

--- games-r-us-source/Hubs/NotificationsHub.cs
+++ games-r-us-source/Hubs/NotificationsHub.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace games_r_us_source.Hubs
 {
+    //[Authorize] // <-- might need to remove this if program crashes 
     public class NotificationsHub : Hub<INotificationClient>
     {
         // Send a test notification when user connect to hub
         public override async Task OnConnectedAsync()
         {
-            await Clients.Client(Context.ConnectionId).RecieveNotification(
+            await Clients.Client(Context.ConnectionId).RecieveNotificationAsync(
                 "User-message = " + Context.ConnectionId);
 
             await base.OnConnectedAsync();
@@ -16,12 +18,12 @@ namespace games_r_us_source.Hubs
 
         public async Task SendNotification(string message)
         {
-            await Clients.All.RecieveNotification(message);
+            await Clients.All.RecieveNotificationAsync(message);
         }
     }
 
     public interface INotificationClient
     {
-        Task RecieveNotification(string message);
+        Task RecieveNotificationAsync(string message);
     }
 }

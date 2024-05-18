bfd47e4ac368fa3e4f3985a8816c58734fd70245
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-05-18 13:37:02 +0200

    First working version of Generic API response
    Co-Authored-By: Sam <142818774+Samuel-Loof@users.noreply.github.com>
    Co-Authored-By: Gabriel Wedin <142593473+gabrielwedin@users.noreply.github.com>
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>

--- games-r-us-source/Controllers/ExternalController.cs
+++ games-r-us-source/Controllers/ExternalController.cs
@@ -26,23 +26,47 @@ namespace games_r_us_source.Controllers
             // if pageNr is not entered (null) it is set to 0 automagically by .net
 
             // get 10 listings paginated 
-            Listing[] listings = await _context.Listings
+            Listing[] listings = _context.Listings
                 .OrderBy(l => l.AuctionEnd)
                 .Skip(10 * pageNr)
                 .Take(10)
-                .ToArrayAsync();
+                .ToArray();
 
             GenericResponseDTO[] result = new GenericResponseDTO[listings.Length];
 
             // get top bid foreach listing & owner of listing
             for (int i = 0; i < listings.Length; i++)
             {
-                result[i].HighestBid = BidHelper.GetHighestBidFromListingID(listings[i].ID, _context).Amount;
+                result[i] = new GenericResponseDTO();
+
+                Bid? highestBid = BidHelper.GetHighestBidFromListingID(listings[i].ID, _context);
+
+                // set highestbid to startingprice if listing has no bids 
+                if (highestBid is null)
+                {
+                    result[i].HighestBid = listings[i].StartingPrice;
+                }
+                else
+                {
+                    result[i].HighestBid = highestBid.Amount;
+                }
+
                 result[i].ListingOwner = ApplicationUserHelper.GetAccountFromUserID(
                     listings[i].ApplicationUserID, _context).FullName;
 
                 result[i].Name = listings[i].Name;
-                result[i].ShortDescription = listings[i].Description.Substring(0, 100);
+
+                // if listing has a description take the first 100 chars
+                if (listings[i].Description is not null)
+                {
+                    result[i].ShortDescription = listings[i].Description.Length >= 100 ? 
+                        listings[i].Description.Substring(0, 100) : listings[i].Description.Substring(0);
+                }
+                else // otherwise set to string.Empty 
+                {
+                    result[i].ShortDescription = "";
+                }
+
                 result[i].Platform = listings[i].Platform;
                 result[i].GameCategory = listings[i].GameCategory;
             }
--- games-r-us-source/Data/GenericResponseDTO.cs
+++ games-r-us-source/Data/GenericResponseDTO.cs
@@ -6,7 +6,7 @@
     {
         public string Name { get; set; }
 
-        public string? ShortDescription { get; set; }
+        public string ShortDescription { get; set; }
 
         public Platform Platform { get; set; }
 

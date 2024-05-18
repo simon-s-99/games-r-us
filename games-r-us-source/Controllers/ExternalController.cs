e97d776b4fc8500de49b894e8bd2285d80b60fa5
Simon Sörqvist <simon.t.sorqvist@gmail.com>
2024-05-18 13:47:46 +0200

    6 api (#53)
    * Create APIExternal.cs
    
    created from template item in Visual Studio community
    
    * API External generic response DTO class created
    
    * Revert "API External generic response DTO class created"
    
    This reverts commit c751c0847517b5d44743a55e25451747bbadbcc5.
    
    * API External generic response DTO class created (w/ co-authors)
    
    with co-authors this time
    
    Co-Authored-By: Sam <142818774+Samuel-Loof@users.noreply.github.com>
    Co-Authored-By: Gabriel Wedin <142593473+gabrielwedin@users.noreply.github.com>
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * API External | Get generic Listings response (not working)
    
    Co-Authored-By: Sam <142818774+Samuel-Loof@users.noreply.github.com>
    Co-Authored-By: Gabriel Wedin <142593473+gabrielwedin@users.noreply.github.com>
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * Got program.cs updates from pr #46
    
    Got code snippet from pr #46 for AddingControllers with correct middleware.
    
    Co-Authored-By: Sam <142818774+Samuel-Loof@users.noreply.github.com>
    Co-Authored-By: Gabriel Wedin <142593473+gabrielwedin@users.noreply.github.com>
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * APIExternal -> ExternalController rename
    
    Co-Authored-By: Sam <142818774+Samuel-Loof@users.noreply.github.com>
    Co-Authored-By: Gabriel Wedin <142593473+gabrielwedin@users.noreply.github.com>
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * First working version of Generic API response
    
    Co-Authored-By: Sam <142818774+Samuel-Loof@users.noreply.github.com>
    Co-Authored-By: Gabriel Wedin <142593473+gabrielwedin@users.noreply.github.com>
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * External API | Generic response returns category as string
    
    Now returns category and platform as string instead of of enum (enum returned ints in the json response).
    
    Co-Authored-By: Sam <142818774+Samuel-Loof@users.noreply.github.com>
    Co-Authored-By: Gabriel Wedin <142593473+gabrielwedin@users.noreply.github.com>
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    ---------
    
    Co-authored-by: Sam <142818774+Samuel-Loof@users.noreply.github.com>
    Co-authored-by: Gabriel Wedin <142593473+gabrielwedin@users.noreply.github.com>
    Co-authored-by: Adam Kumlin <kumlinadam99@gmail.com>

---
+++ games-r-us-source/Controllers/ExternalController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using games_r_us_source.Data;
+using games_r_us_source.Components.Helpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace games_r_us_source.Controllers
+{
+    // change route name if we add more api's in the future 
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExternalController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExternalController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Returns ten listings at a time with their associated owner and highest bid.
+        /// </summary>
+        [HttpGet("listings")] // TODO: Add url to response so that receiver can navigate to our website. 
+        public async Task<ActionResult<IEnumerable<GenericResponseDTO>>> GetListings([FromQuery] int pageNr = 0)
+        {
+            // if pageNr is not entered (null) it is set to 0 automagically by .net
+
+            // get 10 listings paginated 
+            Listing[] listings = _context.Listings
+                .OrderBy(l => l.AuctionEnd)
+                .Skip(10 * pageNr)
+                .Take(10)
+                .ToArray();
+
+            GenericResponseDTO[] result = new GenericResponseDTO[listings.Length];
+
+            // get top bid foreach listing & owner of listing
+            for (int i = 0; i < listings.Length; i++)
+            {
+                result[i] = new GenericResponseDTO();
+
+                Bid? highestBid = BidHelper.GetHighestBidFromListingID(listings[i].ID, _context);
+
+                // set highestbid to startingprice if listing has no bids 
+                if (highestBid is null)
+                {
+                    result[i].HighestBid = listings[i].StartingPrice;
+                }
+                else
+                {
+                    result[i].HighestBid = highestBid.Amount;
+                }
+
+                result[i].ListingOwner = ApplicationUserHelper.GetAccountFromUserID(
+                    listings[i].ApplicationUserID, _context).FullName;
+
+                result[i].Name = listings[i].Name;
+
+                // if listing has a description take the first 100 chars
+                if (listings[i].Description is not null)
+                {
+                    result[i].ShortDescription = listings[i].Description.Length >= 100 ? 
+                        listings[i].Description.Substring(0, 100) : listings[i].Description.Substring(0);
+                }
+                else // otherwise set to string.Empty 
+                {
+                    result[i].ShortDescription = "";
+                }
+
+                result[i].Platform = listings[i].Platform.ToString();
+                result[i].GameCategory = listings[i].GameCategory.ToString();
+            }
+
+            return Ok(result);
+        }
+    }
+}

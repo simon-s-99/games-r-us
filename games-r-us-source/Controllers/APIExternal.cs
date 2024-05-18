f130efb4bf59a0aea030ebac528224a442ab8eeb
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-05-18 11:22:10 +0200

    Create APIExternal.cs
    created from template item in Visual Studio community

---
+++ games-r-us-source/Controllers/APIExternal.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace games_r_us_source.Controllers
+{
+    // change route name if we add more api's in the future 
+    [Route("api")]
+    [ApiController]
+    public class APIExternal : ControllerBase
+    {
+        // GET
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

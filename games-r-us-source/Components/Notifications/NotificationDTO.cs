3fcb1ba592f035d224b7d50bc71f4ad69ddf413b
Simon Sörqvist <simon.t.sorqvist@gmail.com>
2024-05-22 21:43:10 +0200

    13 notification system (#69) (w/ minor bugs)
    * Add SignalR, create empty hub to work from
    
    * Create basic PoC of notifications system
    
    Co-Authored-By: Simon Sörqvist <simon.t.sorqvist@gmail.com>
    
    * Add methods to handle notifications
    
    Co-Authored-By: Simon Sörqvist <simon.t.sorqvist@gmail.com>
    
    * Removed dapper dependency
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * Update Home.razor
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * Revert "Add methods to handle notifications"
    
    This reverts commit 4554e636dc2685207f36f9597efc4609c75203cd.
    
    * Revert "Update Home.razor"
    
    This reverts commit b720473963cbd8e404bab1e592626daa253de3f1.
    
    * Home.razor | removed clutter in the code
    
    * Sending test "messages" now works (still needs identity)
    
    still needs identity check to send to a specific user
    
    * RecieveNotification -> RecieveNotificationAsync
    
    changed to async naming (it was already a task capable of being run asynchronously)
    
    * Now passing username to SignalR hub via headers
    
    * First message sent from another client
    
    * Program.cs | Logout now works again (images broken)
    
    But images are broken (as expected from this fix).
    
    * Program.cs | logout bug fixed (images work)
    
    Logout bug now fixed & images display properly aswell.
    
    * added null check to test-code
    
    * fix spelling on ReceiveNotificationAsync
    
    Recieve -> Receive
    
    * comments +/-
    
    * MessagingDTO -> NotificationDTO rename
    
    * Add NotificationState to pass state to ancestors
    
    * NotificationState | Change from Singleton to Scoped
    
    Changed in program.cs
    
    * Test-message now works with NotificationState
    
    * + comments
    
    * Sending test messages now work from other components
    
    The implementation will be a little bloated but the alternative would make the code really hard to understand.
    
    * Moving notification logic to one component
    
    the component will be mounted in Components.Layout making sure that each logged in user always has a valid hubconnection and is mapped to their username by the notificationhub (this is the goal at least)
    
    * Moved userlogo to images sub-folder
    
    * added notification bell SVG's
    
    * Notification bell button base structure
    
    * BidPlaceForm | User notified when outbid
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * NotificationBell | Now toggles list of notifications
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * NotificationBell | Newnotificationbell icon now toggles
    
    now toggles correctly
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * Update Counter.razor
    
    * BidPlaceForm | Bugfix: notification to null user
    
    Do not send notification to owner of previously highest bid if listing has no highestbid (i.e. listing is at starting price).
    
    ---------
    
    Co-authored-by: Adam Kumlin <skola.adamkumlin99@gmail.com>
    Co-authored-by: Adam Kumlin <kumlinadam99@gmail.com>

---
+++ games-r-us-source/Components/Notifications/NotificationDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace games_r_us_source.Components.Notifications
+{
+    public class NotificationDTO
+    {
+        // This class is only used to send messages to our NotificationsHub
+        // since sending multiple parameters to a method via SignalR
+        // syntax is either impossible or really janky otherwise. 
+
+        public string UserName { get; set; }
+        public string Message { get; set; }
+
+        public NotificationDTO(string userName, string message)
+        {
+            UserName = userName;
+            Message = message;
+        }
+    }
+}
